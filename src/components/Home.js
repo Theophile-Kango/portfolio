42693471c484ef408ee7c44c9c4e0ddae46c0f58
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-18 19:38:58 +0200

    Add responsive design for the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,31 @@
 import React from 'react';
-import styles from './styles/homeStyles.module.scss'
+import skills from '../constant/skills';
+import styles from './styles/homeStyles.module.scss';
 
 const Home = () => {
+    
     return (
         <section id="home" className={styles.home} >
             <h1>Successful Full Stack Web Development</h1>
             <h3>
                 Hi. I’m Theophile, a Full Stack Web Developer 
-                with 1-year experience creating successful web applications with 
-                
-                <a 
-                    href="https://www.ruby-lang.org/en/" 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                > 
-                    Ruby 
-                </a>
-                <a 
-                    href="https://rubyonrails.org/" 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >  
-                    Rails 
-                </a>
-                <a 
-                    href="https://www.javascript.com/" 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                > 
-                    JavaScript 
-                </a>
-                <a 
-                    href="https://reactjs.org/" 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    React
-                </a>
-                <a 
-                    href="https://redux.js.org/" 
-                    target="_blank"
-                    rel="noopener noreferrer"
-                > 
-                    Redux 
-                </a>
-            </h3>
+                with 1-year experience creating successful web applications with
+            </h3> 
+            <ul>
+                {
+                    skills.map(skill => (
+                        <li key={skill.tech}>
+                            <a 
+                                href={skill.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            > 
+                                {skill.tech}
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul> 
             <p>Looking for new opportunities</p>
         </section>
     )
