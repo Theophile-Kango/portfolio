8ccf63446297e9ce98c3427f1f8caa5c8329b1a2
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-18 04:36:36 +0200

    Add data to the contact component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,6 +4,24 @@ const Articles = () => {
     return (
         <section id="home">
             <h1>Articles</h1>
+            <p>
+                <a 
+                    href="https://hackernoon.com/uploading-files-into-ruby-on-rails-activestorage-gv1r3ukr"
+                    target="_blank"
+                    rel="noopener noreferrer" 
+                >
+                    Uploading Files Into Ruby On Rails ActiveStorage
+                </a>
+            </p>
+            <p>
+                <a 
+                    href="https://hackernoon.com/flexbox-vs-css-grid-or-what-should-you-use-as-basic-css-layout-2aai36fm"
+                    target="_blank"
+                    rel="noopener noreferrer" 
+                >
+                    Flexbox Vs. CSS Grid or What Should You Use as Basic CSS Layout
+                </a>
+            </p>
         </section>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import contacts from '../constant/contacts'
 
 const Contact = () => {
+    
     return (
         <section id="contact">
             <h1>Contact</h1>
+            <ul>
+                {
+                    contacts.map((contact, i ) => ( 
+                        <li key={i}>
+                            <a
+                                href={contact.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FontAwesomeIcon icon={contact.icon} />
+                            </a>
+                        </li>
+                    ))
+                }   
+            </ul>
         </section>
     )
 }
