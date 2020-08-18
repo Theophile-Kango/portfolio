ddb5a40b1eb11ad144a8d243b78cd7736e665484
Theophile-Kango <fadhili.kango@gmail.com>
2020-08-18 12:38:01 +0200

    Add style to the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,47 @@ import styles from './styles/homeStyles.module.scss'
 
 const Home = () => {
     return (
-        <section id="home" className={styles.section} >
+        <section id="home" className={styles.home} >
             <h1>Successful Full Stack Web Development</h1>
             <h3>
                 Hi. I’m Theophile, a Full Stack Web Developer 
                 with 1-year experience creating successful web applications with 
-                <span>Ruby </span>
-                <span>Ruby On Rails </span>
-                <span>JavaScript </span>
-                <span>React|Redux</span>
+                
+                <a 
+                    href="https://www.ruby-lang.org/en/" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                > 
+                    Ruby 
+                </a>
+                <a 
+                    href="https://rubyonrails.org/" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >  
+                    Rails 
+                </a>
+                <a 
+                    href="https://www.javascript.com/" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                > 
+                    JavaScript 
+                </a>
+                <a 
+                    href="https://reactjs.org/" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    React
+                </a>
+                <a 
+                    href="https://redux.js.org/" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                > 
+                    Redux 
+                </a>
             </h3>
             <p>Looking for new opportunities</p>
         </section>
