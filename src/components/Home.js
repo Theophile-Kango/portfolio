e76c7100afd033cb10032b98fe5a700e04f36ef8
Theophile-Kango <fadhili.kango@gmail.com>
2021-01-25 17:36:43 +0200

    Change the bg color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import styles from './styles/homeStyles.module.scss';
+import React from "react";
+import styles from "./styles/homeStyles.module.scss";
 
 const Home = () => (
   <section id="home" className={styles.home}>
     <h1>Hi, I&apos;m Theophile</h1>
-    <h3>
-      Full-Stack Remote Developer
-    </h3>
+    <h3>Full-Stack Software Developer</h3>
     <p>
-      Certified web developer who specializes in working with
-      Ruby, Rails, React, Javascript and Redux.
-      Currently exploring front-end remote development opportunities
+      Certified web developer who specializes in working with Ruby, Rails,
+      React, Javascript and Redux. Currently exploring front-end remote
+      development opportunities
     </p>
   </section>
 );
