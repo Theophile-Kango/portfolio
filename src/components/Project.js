41b8a891f99be92031e1b3c0d5611cf493ba28db
Theophile-Kango <fadhili.kango@gmail.com>
2021-01-27 09:49:03 +0200

    Change the style on the project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import styles from './styles/projectStyles.module.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import styles from "./styles/projectStyles.module.scss";
 
 AOS.init({
   offset: 400,
@@ -12,34 +12,34 @@ AOS.init({
 });
 
 const Project = ({ object }) => {
-  const {
-    title, details, tech, demo, image,
-  } = object;
+  const { title, details, tech, demo, image } = object;
   const { src, imageTitle } = image;
 
   return (
     <article className={styles.project} data-aos="fade-left">
-      <h3>{title}</h3>
-      <p>{details}</p>
-      <img src={src} alt={imageTitle} />
-      <h4>
-        Built with:
-        {tech.map(elt => (
-          <span key={elt}>{elt}</span>
+      <div>
+        <img src={src} alt={imageTitle} />
+      </div>
+      <div>
+        <h3>{title}</h3>
+        <p>{details}</p>
+        <h4>
+          Built with:
+          {tech.map((elt) => (
+            <span key={elt}>{elt}</span>
+          ))}
+        </h4>
+        {demo.map((elt) => (
+          <a
+            target="_blank"
+            href={elt.link}
+            key={elt.link}
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={elt.icon} />
+          </a>
         ))}
-      </h4>
-      {
-                demo.map(elt => (
-                  <a
-                    target="_blank"
-                    href={elt.link}
-                    key={elt.link}
-                    rel="noopener noreferrer"
-                  >
-                    <FontAwesomeIcon icon={elt.icon} />
-                  </a>
-                ))
-            }
+      </div>
     </article>
   );
 };
