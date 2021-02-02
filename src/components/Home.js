6232c4bd8d78fa550972f9d5ea908fd5f88bc362
Theophile-Kango <fadhili.kango@gmail.com>
2021-02-02 16:09:48 +0200

    Fix the capitalization and punctuation of technical terms

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './styles/homeStyles.module.scss';
-import Contact from './Contact';
+import React from "react";
+import styles from "./styles/homeStyles.module.scss";
+import Contact from "./Contact";
 
 const Home = () => (
   <section className={styles.home} id="home">
@@ -9,7 +9,7 @@ const Home = () => (
     <h3>Full-Stack Software Developer</h3>
     <p>
       Certified web developer who specializes in working with Ruby, Rails,
-      React, Javascript and Redux. Currently exploring front-end remote
+      React, JavaScript and Redux. Currently exploring front-end remote
       development opportunities
     </p>
     <div />
