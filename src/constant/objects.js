df5f4dd78670b3f19d0071a2f25e0723753ebe5e
Theophile-Kango <fadhili.kango@gmail.com>
2021-02-04 08:29:10 +0200

    Correct some typo and replace the resume

--- src/constant/objects.js
+++ src/constant/objects.js
@@ -1,121 +1,121 @@
-import { faCode, faVideo, faLink } from '@fortawesome/free-solid-svg-icons';
-import CoursesTrack from '../img/courses-track.png';
-import FamilyMovies from '../img/family-movies.png';
-import NewsWeek from '../img/news-week.png';
-import SpaceShooterGame from '../img/space-shooter-game.png';
-import TelegramBot from '../img/telegram-bot.png';
+import { faCode, faVideo, faLink } from "@fortawesome/free-solid-svg-icons";
+import CoursesTrack from "../img/courses-track.png";
+import FamilyMovies from "../img/family-movies.png";
+import NewsWeek from "../img/news-week.png";
+import SpaceShooterGame from "../img/space-shooter-game.png";
+import TelegramBot from "../img/telegram-bot.png";
 
 const objects = [
   {
-    title: 'Courses Track App',
+    title: "Courses Track App",
     details: `The project is about grouping hours spent on different courses 
         at the University with a mobile web app`,
-    tech: ['Ruby On Rails', 'HTML5', 'SCSS'],
+    tech: ["Ruby on Rails", "HTML5", "SCSS"],
     demo: [
       {
-        link: 'https://github.com/Theophile-Kango/group_our_transactions',
+        link: "https://github.com/Theophile-Kango/group_our_transactions",
         icon: faCode,
       },
       {
-        link: 'https://www.youtube.com/watch?v=pP40lWlStKE&feature=youtu.be',
+        link: "https://www.youtube.com/watch?v=pP40lWlStKE&feature=youtu.be",
         icon: faVideo,
       },
       {
-        link: 'https://courses-track.herokuapp.com/',
+        link: "https://courses-track.herokuapp.com/",
         icon: faLink,
       },
     ],
     image: {
       src: CoursesTrack,
-      imageTitle: 'Courses Track',
+      imageTitle: "Courses Track",
     },
   },
   {
-    title: 'Family Movies',
+    title: "Family Movies",
     details: `A responsive catalogue of family-friendly movies that allows 
       users to filter and pull up movie details using the IMDB API`,
-    tech: ['React', 'Redux', 'Node.js', 'ES6', 'SCSS'],
+    tech: ["React", "Redux", "Node.js", "SCSS"],
     demo: [
       {
-        link: 'https://github.com/Theophile-Kango/family-movies-catalogue',
+        link: "https://github.com/Theophile-Kango/family-movies-catalogue",
         icon: faCode,
       },
       {
-        link: 'https://www.loom.com/share/6968264108d94ceb967648e8a3f40b2c',
+        link: "https://www.loom.com/share/6968264108d94ceb967648e8a3f40b2c",
         icon: faVideo,
       },
       {
-        link: 'https://family-movies-catalogue.netlify.app/',
+        link: "https://family-movies-catalogue.netlify.app/",
         icon: faLink,
       },
     ],
     image: {
       src: FamilyMovies,
-      imageTitle: 'Family Movies',
+      imageTitle: "Family Movies",
     },
   },
   {
-    title: 'News Week',
+    title: "News Week",
     details: `Building a web page that is similar to the newsweek web page 
             with responsive web design`,
-    tech: ['HTML5', 'CSS3', 'Bootstrap 4'],
+    tech: ["HTML5", "CSS3", "Bootstrap 4"],
     demo: [
       {
-        link: 'https://github.com/Theophile-Kango/newsweek',
+        link: "https://github.com/Theophile-Kango/newsweek",
         icon: faCode,
       },
       {
-        link: 'https://theophile-kango.github.io/newsweek/',
+        link: "https://theophile-kango.github.io/newsweek/",
         icon: faLink,
       },
     ],
     image: {
       src: NewsWeek,
-      imageTitle: 'News Week',
+      imageTitle: "News Week",
     },
   },
   {
-    title: 'Space Shooter Game',
+    title: "Space Shooter Game",
     details: `This project is about the implementation of a space mission 
         where the player's goal is to prevent meteors from reaching the earth`,
-    tech: ['JavaScript (ES6)', 'webpack', 'Node.js', 'Phaser 3'],
+    tech: ["JavaScript", "webpack", "Node.js", "Phaser 3"],
     demo: [
       {
-        link: 'https://github.com/Theophile-Kango/space-shooter-game',
+        link: "https://github.com/Theophile-Kango/space-shooter-game",
         icon: faCode,
       },
       {
-        link: 'https://space-game-mission.netlify.app/',
+        link: "https://space-game-mission.netlify.app/",
         icon: faLink,
       },
     ],
     image: {
       src: SpaceShooterGame,
-      imageTitle: 'Space Shooter Game',
+      imageTitle: "Space Shooter Game",
     },
   },
   {
-    title: 'Telegram Bot',
+    title: "Telegram Bot",
     details: `This project is about a telegram bot that can help you with corona-virus info
         with each given Country`,
-    tech: ['Ruby'],
+    tech: ["Ruby"],
     demo: [
       {
-        link: 'https://github.com/Theophile-Kango/ruby_capston_project',
+        link: "https://github.com/Theophile-Kango/ruby_capston_project",
         icon: faCode,
       },
       {
-        link: 'https://youtu.be/q4V9cXcDkII',
+        link: "https://youtu.be/q4V9cXcDkII",
         icon: faVideo,
       },
       {
-        link: 'https://t.me/covidis_info_bot',
+        link: "https://t.me/covidis_info_bot",
         icon: faLink,
       },
     ],
     image: {
       src: TelegramBot,
-      imageTitle: 'Telegram Bot',
+      imageTitle: "Telegram Bot",
     },
   },
 ];
